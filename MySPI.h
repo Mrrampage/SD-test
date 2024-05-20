c17ee834f11bc192a2b25a4a1e21b0f0d8a7adb3
Mrrampage <36160434+Mrrampage@users.noreply.github.com>
2024-05-20 21:56:39 +0800

    Add files via upload

---
+++ MySPI.h
@@ -0,0 +1,10 @@
+#ifndef __MYSPI_H
+#define __MYSPI_H
+#include "stm32f10x.h"                  // Device header
+
+/******************************************************************************************/
+void SPI1_Init(void);			 //初始化SPI口
+void SPI1_SetSpeed(u8 SpeedSet); //设置SPI速度   
+u8 SPI1_ReadWriteByte(u8 TxData);//SPI总线读写一个字节
+		 
+#endif
---
+++ SPI_SD.h
@@ -0,0 +1,111 @@
+#ifndef __SPI__SD_H
+#define __SPI__SD_H
+#include "stm32f10x.h"                  // Device header
+
+
+#define BITBAND(addr, bitnum) ((addr & 0xF0000000)+0x2000000+((addr &0xFFFFF)<<5)+(bitnum<<2)) 
+#define MEM_ADDR(addr)  *((volatile unsigned long  *)(addr)) 
+#define BIT_ADDR(addr, bitnum)   MEM_ADDR(BITBAND(addr, bitnum)) 
+//IO口地址映射
+#define GPIOA_ODR_Addr    (GPIOA_BASE+12) //0x4001080C 
+#define GPIOB_ODR_Addr    (GPIOB_BASE+12) //0x40010C0C 
+#define GPIOC_ODR_Addr    (GPIOC_BASE+12) //0x4001100C 
+#define GPIOD_ODR_Addr    (GPIOD_BASE+12) //0x4001140C 
+#define GPIOE_ODR_Addr    (GPIOE_BASE+12) //0x4001180C 
+#define GPIOF_ODR_Addr    (GPIOF_BASE+12) //0x40011A0C    
+#define GPIOG_ODR_Addr    (GPIOG_BASE+12) //0x40011E0C    
+
+#define GPIOA_IDR_Addr    (GPIOA_BASE+8) //0x40010808 
+#define GPIOB_IDR_Addr    (GPIOB_BASE+8) //0x40010C08 
+#define GPIOC_IDR_Addr    (GPIOC_BASE+8) //0x40011008 
+#define GPIOD_IDR_Addr    (GPIOD_BASE+8) //0x40011408 
+#define GPIOE_IDR_Addr    (GPIOE_BASE+8) //0x40011808 
+#define GPIOF_IDR_Addr    (GPIOF_BASE+8) //0x40011A08 
+#define GPIOG_IDR_Addr    (GPIOG_BASE+8) //0x40011E08 
+ 
+//IO口操作,只对单一的IO口!
+//确保n的值小于16!
+#define PAout(n)   BIT_ADDR(GPIOA_ODR_Addr,n)  //输出 
+#define PAin(n)    BIT_ADDR(GPIOA_IDR_Addr,n)  //输入 
+
+#define PBout(n)   BIT_ADDR(GPIOB_ODR_Addr,n)  //输出 
+#define PBin(n)    BIT_ADDR(GPIOB_IDR_Addr,n)  //输入 
+
+#define PCout(n)   BIT_ADDR(GPIOC_ODR_Addr,n)  //输出 
+#define PCin(n)    BIT_ADDR(GPIOC_IDR_Addr,n)  //输入 
+
+#define PDout(n)   BIT_ADDR(GPIOD_ODR_Addr,n)  //输出 
+#define PDin(n)    BIT_ADDR(GPIOD_IDR_Addr,n)  //输入 
+
+#define PEout(n)   BIT_ADDR(GPIOE_ODR_Addr,n)  //输出 
+#define PEin(n)    BIT_ADDR(GPIOE_IDR_Addr,n)  //输入
+
+#define PFout(n)   BIT_ADDR(GPIOF_ODR_Addr,n)  //输出 
+#define PFin(n)    BIT_ADDR(GPIOF_IDR_Addr,n)  //输入
+
+#define PGout(n)   BIT_ADDR(GPIOG_ODR_Addr,n)  //输出 
+#define PGin(n)    BIT_ADDR(GPIOG_IDR_Addr,n)  //输入
+
+// SD卡类型定义  
+#define SD_TYPE_ERR     0X00
+#define SD_TYPE_MMC     0X01
+#define SD_TYPE_V1      0X02
+#define SD_TYPE_V2      0X04
+#define SD_TYPE_V2HC    0X06	   
+// SD卡指令表  	   
+#define CMD0    0       //卡复位
+#define CMD1    1
+#define CMD8    8       //命令8 ，SEND_IF_COND
+#define CMD9    9       //命令9 ，读CSD数据
+#define CMD10   10      //命令10，读CID数据
+#define CMD12   12      //命令12，停止数据传输
+#define CMD16   16      //命令16，设置SectorSize 应返回0x00
+#define CMD17   17      //命令17，读sector
+#define CMD18   18      //命令18，读Multi sector
+#define CMD23   23      //命令23，设置多sector写入前预先擦除N个block
+#define CMD24   24      //命令24，写sector
+#define CMD25   25      //命令25，写Multi sector
+#define CMD41   41      //命令41，应返回0x00
+#define CMD55   55      //命令55，应返回0x01
+#define CMD58   58      //命令58，读OCR信息
+#define CMD59   59      //命令59，使能/禁止CRC，应返回0x00
+//数据写入回应字意义
+#define MSD_DATA_OK                0x05
+#define MSD_DATA_CRC_ERROR         0x0B
+#define MSD_DATA_WRITE_ERROR       0x0D
+#define MSD_DATA_OTHER_ERROR       0xFF
+//SD卡回应标记字
+#define MSD_RESPONSE_NO_ERROR      0x00
+#define MSD_IN_IDLE_STATE          0x01
+#define MSD_ERASE_RESET            0x02
+#define MSD_ILLEGAL_COMMAND        0x04
+#define MSD_COM_CRC_ERROR          0x08
+#define MSD_ERASE_SEQUENCE_ERROR   0x10
+#define MSD_ADDRESS_ERROR          0x20
+#define MSD_PARAMETER_ERROR        0x40
+#define MSD_RESPONSE_FAILURE       0xFF
+ 							   						 	 
+//这部分应根据具体的连线来修改!
+//MiniSTM32开发板使用的是PA3作为SD卡的CS脚.
+#define	SD_CS  PAout(4) 	//SD卡片选引脚					    	  
+
+extern uint8_t  SD_Type;			//SD卡的类型
+//函数申明区 
+uint8_t SD_SPI_ReadWriteByte(uint8_t data);
+void SD_SPI_SpeedLow(void);
+void SD_SPI_SpeedHigh(void);
+uint8_t SD_WaitReady(void);							//等待SD卡准备
+uint8_t SD_GetResponse(uint8_t Response);					//获得相应
+uint8_t SD_Initialize(void);							//初始化
+uint8_t SD_ReadDisk(uint8_t*buf,uint32_t sector,uint8_t cnt);		//读块
+uint8_t SD_WriteDisk(uint8_t*buf,uint32_t sector,uint8_t cnt);		//写块
+uint32_t SD_GetSectorCount(void);   					//读扇区数
+uint8_t SD_GetCID(uint8_t *cid_data);                     //读SD卡CID
+uint8_t SD_GetCSD(uint8_t *csd_data);                     //读SD卡CSD
+ 
+#endif
+
+
+
+
+
